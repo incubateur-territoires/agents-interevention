a62cfa5d0b45256f907ac4e1e491f59e845b4ff3
Samantha Dangu <56520764+SamanthaDangu@users.noreply.github.com>
2023-02-01 18:20:29 +0100

    templating de la page d'une intervention

--- pages/interventions/[id].vue
+++ pages/interventions/[id].vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    <SubHeader>Intervention 1234</SubHeader>
-    <Stepper></Stepper>
-  </div>
+
+  <SubHeader>Intervention 1234</SubHeader>
+  <Stepper></Stepper>
+  <article>
+    <p> Kevin DUPOND, Stéphane Durand</p>
+    <Tag>Urgent</Tag>
+    <p class="fr-icon-map-pin-2-line"> 16, route de Béthune, 62300 Lens</p>
+    <p class="fr-icon-calendar-2-line"> Publiée le 23/11/2023 par Jean-Baptiste M.</p>
+    <p class="fr-icon-space"> Taille : Petit</p>
+    <p class="fr-icon-edit-box-line"> Support : Mur, Porte</p>
+    <p> Le tag est situé sur la porte de garage et sur le mur situé juste à sa gauche. Il y a 4m2 de tags à nettoyer
+      environ.</p>
+  </article>
+  <img class="fr-responsive-img" src="https://i.pinimg.com/originals/fe/64/99/fe649940d2ea4daa02c5f942fbb88517.jpg"
+    alt="[À MODIFIER - vide ou texte alternatif de l’image]" />
+<hr>
+  <InterventionComments />
+
+  <button class="fr-btn"> <a href="/interventions/1234">
+      Commencer l'intervention
+    </a>
+  </button>
 </template>
