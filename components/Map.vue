29c3f5dc5d9793ca6c54c155716a2f3e53bcd305
Vincent Agnano <vinyll@protonmail.com>
2023-02-02 09:50:22 +0100

    Ajout du composant de carto basé sur Vue Leaflet

---
+++ components/Map.vue
@@ -0,0 +1,19 @@
+<script setup>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+const zoom = 14;
+</script>
+
+<template>
+  <div style="height: 100%; width: 100%; min-height: 50px;">
+    <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      ></l-tile-layer>
+      <slot></slot>
+    </l-map>
+  </div>
+</template>
