61c9660014b75daaae2fb90c64b084e390ed6157
Samantha Dangu <56520764+SamanthaDangu@users.noreply.github.com>
2023-02-02 18:05:38 +0100

    Style sur le bouton commencer l'intervention

--- pages/interventions/[id].vue
+++ pages/interventions/[id].vue
@@ -3,8 +3,16 @@
   <SubHeader>Intervention 1234</SubHeader>
   <InterventionDetails />
 
-  <button class="fr-btn"> <a href="/interventions/1234">
+  <button class="fr-btn fr-grid-row--center"> <a href="/interventions/1234">
       Commencer l'intervention
     </a>
   </button>
 </template>
+
+<style scoped>
+button {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+}
+</style>
