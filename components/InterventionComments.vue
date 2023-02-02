ba05cea3e3bb0d76c498cd281005ba783f4abd85
Samantha Dangu <56520764+SamanthaDangu@users.noreply.github.com>
2023-02-02 18:08:13 +0100

    Style sur les commentaires (à finir)

--- components/InterventionComments.vue
+++ components/InterventionComments.vue
@@ -1,79 +1,75 @@
 <template>
-    <h4>Commentaires</h4>
-    <section>
-        <aside>
+    <h2 class="fr-pr-2w fr-pl-2w">Commentaires</h2>
+    <section class="fr-p-2w">
+        <!-- <aside>
             Aucun commentaire
-        </aside>
+        </aside> -->
 
         <section>
             <ul>
-                <li class="fr-responsive-img fr-p-1w fr-col-10 comment-agent">
+                <li class="fr-responsive-img fr-col-11 fr-mb-1w fr-grid-row--middle comment-agent">
                     <img class="fr-p-1w" src="https://cdn.pixabay.com/photo/2015/07/20/13/01/man-852770_960_720.jpg"
                         alt="">
                     <div>
-                        <p class="fr-p-2w">
+                        <p class="fr-p-2w fr-mb-1w">
                             Impossible de nettoyer la partie droite, il faut revenir avec plus de détergent.
                         </p>
                         <datetime>01-02-2023</datetime>
                     </div>
                 </li>
 
-                <li class="fr-responsive-img fr-p-1w fr-col-10 comment-collectivite">
+                <li class="fr-responsive-img fr-col-11 fr-mb-1w fr-grid-row--middle comment-collectivite">
                     <img class="fr-p-1w" src="https://cdn.pixabay.com/photo/2022/06/25/01/46/woman-7282714_960_720.png"
                         alt="">
-                    <div>
-                        <p class="fr-p-2w">
+                    <div class="fr-grid-row--right">
+                        <p class="fr-p-2w fr-mb-1w">
                             Ok, on en rachète, on le recevra dans 3 jours.
+                        </p>
+                        <datetime class="fr-col-4">01-02-2023</datetime>
+                    </div>
+                </li>
+            </ul>
+            <h3 class="fr-m-2w fr-grid-row--middle">Aujourd'hui</h3>
+            <ul>
+                <li class="fr-responsive-img fr-col-11 fr-mb-1w fr-grid-row--middle comment-agent">
+                    <img class="fr-p-1w" src="https://cdn.pixabay.com/photo/2015/07/20/13/01/man-852770_960_720.jpg"
+                        alt="">
+                    <div>
+                        <p class="fr-p-2w fr-mb-1w">
+                            C’est nettoyé. La mission est terminée.
                         </p>
                         <datetime>01-02-2023</datetime>
                     </div>
                 </li>
             </ul>
         </section>
-        <form>
-            <textarea id="comment" rows="3" cols="50" placeholder="Ecrire un message" />
-            <button class="fr-icon-send-plane-fill" type="submit"></button>
+        <form class="fr-p-2w comment">
+            <textarea class="fr-col-10 fr-p-1w" placeholder="Ecrire un message" />
+            <button class="fr-icon-send-plane-fill comment-button" type="submit"></button>
         </form>
     </section>
 
 </template>
 
 <style scoped>
+li {
+    list-style: none;
+    display: flex;
+}
+
 img {
     max-width: 75px;
     max-height: 75px;
     border-radius: 100%;
 }
 
-ul,
-li,
-p {
-    padding: 0;
-    margin: 0;
-}
-
-ul {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-}
-
-li {
-    list-style: none;
-    display: flex;
-    align-items: center;
-}
-
 li.comment-agent p {
     background-color: var(--background-alt-blue-france);
+    border-radius: 16px 16px 16px 0px;
 }
 
 li.comment-collectivite {
     flex-direction: row-reverse;
-}
-li.comment-collectivite p {
-    box-shadow: 0 0 0 1px var(--border-default-grey);
 }
 
 li.comment-collectivite div {
@@ -82,7 +78,30 @@ li.comment-collectivite div {
     align-items: flex-end;
 }
 
-p {
-    border-radius: 16px;
+li.comment-collectivite p {
+    box-shadow: 0 0 0 1px var(--border-default-grey);
+    border-radius: 16px 16px 0px 16px;
+}
+
+textarea{
+    box-shadow: 0 0 0 1px var(--border-default-grey);
+    border-radius: 10px;
+}
+
+button.comment-button{
+    color: var(--background-action-high-blue-france);
+}
+
+h3 {
+    background-color: var(--border-default-grey);
+    border-radius: 10px;
+    display: inline-block;
+}
+h3:after{
+    content: "-";
+}
+
+h3:before{
+    content: "-";
 }
 </style>
