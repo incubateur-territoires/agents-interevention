95962f66c3852b9b260b30f761de7400b65e8d95
Samantha Dangu <56520764+SamanthaDangu@users.noreply.github.com>
2023-02-03 11:07:40 +0100

    Integration de la carte

--- components/InterventionDetails.vue
+++ components/InterventionDetails.vue
@@ -1,3 +1,6 @@
+<script setup>
+import { LMarker, LPopup, LTooltip } from "@vue-leaflet/vue-leaflet";
+</script>
 <template>
     <Stepper></Stepper>
     <article class="fr-p-2w">
@@ -7,13 +10,27 @@
         <p class="fr-icon-calendar-2-line fr-pt-1w"> Publiée le 23/11/2023 par Jean-Baptiste M.</p>
         <p class="fr-icon-space fr-pt-1w"> Taille : Petit</p>
         <p class="fr-icon-edit-box-line fr-pt-1w"> Support : Mur, Porte</p>
-        <p class="fr-mt-3w fr-mb-3w"> Le tag est situé sur la porte de garage et sur le mur situé juste à sa gauche. Il y a 4m2
+        <p class="fr-mt-3w fr-mb-3w"> Le tag est situé sur la porte de garage et sur le mur situé juste à sa gauche. Il
+            y a 4m2
             de tags à nettoyer environ.</p>
-    <slot />
-        <img class="fr-responsive-img"
+        <slot />
+        <img class="fr-responsive-img fr-mb-2w"
             src="https://i.pinimg.com/originals/fe/64/99/fe649940d2ea4daa02c5f942fbb88517.jpg"
             alt="[À MODIFIER - vide ou texte alternatif de l’image]" />
+        <Map :lat-lng="[50.433082, 2.821937]" :zoom=(14) style="height:500px">
+            <LMarker :lat-lng="[50.433082, 2.821937]">
+                <LTooltip></LTooltip>
+                <LPopup>Intervention 1234, Tag
+                    <button>
+                        <a href="/interventions/1234">
+                            Voir l'intervention
+                        </a>
+                    </button>
+                </LPopup>
+            </LMarker>
+        </Map>
     </article>
     <hr>
     <InterventionComments />
 </template>
+
