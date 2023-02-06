f9be702b360e5ffa33478c0233fcf059965d58d3
Samantha Dangu <dangu.samantha@gmail.com>
2023-02-06 12:07:02 +0100

    Revert "modification route bouton commencer"
    This reverts commit 4d3c34b259734ba371c006f4ccf5a550123ce694.

--- pages/interventions/[id].vue
+++ pages/interventions/[id].vue
@@ -4,7 +4,7 @@
   <Stepper :current="1" :last="3" :labels="['À faire', 'En cours', 'Terminé']" />
   <InterventionDetails />
 
-  <button class="fr-btn fr-grid-row--center"> <a href="/interventions/1234/encours">
+  <button class="fr-btn fr-grid-row--center"> <a href="/interventions/1234-en-cours">
       Commencer l'intervention
     </a>
   </button>
