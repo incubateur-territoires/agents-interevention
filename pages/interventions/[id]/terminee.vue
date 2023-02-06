f62a9929eea6e07854aa7a35f9271402ebf04a74
Samantha Dangu <dangu.samantha@gmail.com>
2023-02-06 12:44:50 +0100

    création de la page d'une intervention terminée

---
+++ pages/interventions/[id]/terminee.vue
@@ -0,0 +1,28 @@
+<template>
+  <SubHeader>Intervention 1234</SubHeader>
+  <Stepper :current="3" :last="3" :labels="['À faire', 'En cours', 'Terminé']">
+  </Stepper>
+  <InterventionDetails>
+    <section>
+      <p slot="réalisée" class="fr-icon-checkbox-circle-line">
+        Réalisée le 23/11/2023 par Moussa J.
+      </p>
+      <p slot="technique" class="fr-icon-brush-3-line fr-pt-1w fr-mb-3w">
+        Technique d’effaçage : Hydrogrommage
+      </p>
+    </section>
+  </InterventionDetails>
+  <button class="fr-btn fr-grid-row--center">
+    <a href="/">
+      Revenir à l’accueil
+    </a>
+  </button>
+</template>
+
+<style scoped>
+button {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+}
+</style>
